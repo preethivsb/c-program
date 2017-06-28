528819c8ac9ab777d738a72b98ae894f1df812e0
preethivsb <preethivsb91@gmail.com>
2017-06-28 16:23:23 +0530

    positive negative
    c program

---
+++ positive.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ double no;
+printf("enter a no");
+scanf("%1f",&no);
+if(no<=0.0)
+printf("you enter no is zero");
+else
+printf("you entered no is negative");
+}
+else
+printf("no is positive");
+return 0;
+}
+
