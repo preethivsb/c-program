924bcb61443f2dad6e4036049b756173479e76fa
preethivsb <preethivsb91@gmail.com>
2017-06-29 14:03:09 +0530

    vowel.c
    to find whether the given alphabet is vowel is consonant.

---
+++ vowel.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main();
+{
+int islowercase, isuppercase;
+printf("enter the letter");
+scanf("%c",&c);
+if(islowercase || isuppercase)
+printf("the alphabet isvowel");
+else
+printf("the alphabet is not vowel");
+getch();
+}
