1946fe05353e7a0924a9ac41b91d522fb0b3a389
preethivsb <preethivsb91@gmail.com>
2017-06-29 14:24:41 +0530

    large
    find the largest number

---
+++ large.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a=1,b=2,c=3;
+printf("the largest among 3 numbers");
+if(a>b)||(a>c)
+printf("the largest is a);
+elseif(b>c)
+printf(:the largest is b);
+else
+printf("the largest is c");
+getch();
+}
