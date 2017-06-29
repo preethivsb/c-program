f6333c962bc58dcd5cf767efca4d0261aca5895a
preethivsb <preethivsb91@gmail.com>
2017-06-29 14:12:37 +0530

    alpha
    find whether the input is alphabet or not

---
+++ alpha.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int c;
+printf("enter the input");
+scanf("%d",&c);
+if(c>='a')||(c<='z')
+printf("the input is an alphabet");
+else
+printf("the input is not an alplabet);
+getch();
+}
