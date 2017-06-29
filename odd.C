00f345c29a7e8fe4dd7953f23a009147aa71a57d
preethivsb <preethivsb91@gmail.com>
2017-06-29 13:53:37 +0530

    odd
    to find whether the number is odd or not

---
+++ odd.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int no;
+Print("enter the number ");
+scanf("%d",&no);
+if(no%2==0)
+printf("the number is even");
+else
+Printf("the number is odd");
+retrun 0;
+}
