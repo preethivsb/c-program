df3351c5a64e96271f6427be4a3502c439cc4af6
preethivsb <preethivsb91@gmail.com>
2017-06-30 14:39:14 +0530

    palindrome
    check whether it is palindrome or not

---
+++ palin.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int n,orgint,revint;
+orgint=n;
+printf("the orih=ginal number");
+scanf("%d",&n);
+if(n!=0)
+{
+remainder=nr%10;
+revint=revint*10+remainder;
+n/=10;
+}
+if(orgint==revint)
+printf("the entered number is palindrome");
+else 
+printf("it is not palindrome");
+getch();
+}
