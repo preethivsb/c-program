7669b3eeedb36899a88627de04223bb45cb45ec6
preethivsb <preethivsb91@gmail.com>
2017-07-08 12:16:51 +0530

    oddtwo
    find the odd numbers between the range

---
+++ oddtwo.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n=40,num,m=20;
+clrscr();
+printf("the number between the range");
+for(num=m;num<=n;num++)
+{
+if(num%2==1)
+printf("%d",num);
+}
+getch();
+}
